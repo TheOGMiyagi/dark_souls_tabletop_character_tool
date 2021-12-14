d01822ca962ef0bd357f6dccb58ed56475ae2df5
TheOGMiyagi <69600897+TheOGMiyagi@users.noreply.github.com>
2021-12-13 21:37:16 -0800

    Add files via upload
    Changes:
    - Logging Implemented
    - Readability Changes
    - Driver Code Updated To Allow For The Program To Be Rerun

--- main.py
+++ main.py
@@ -1,95 +1,84 @@
 """<Module "dark_souls_tabletop_character_tool/main.py"
-
 Description:
-
 The Main Module of the project.
-
 """
-
 #import [Module/Package]
-
 import logging
-
 from random import randint as roll
 
+# LOGGER SETUP
+logging.basicConfig(filename='scriptlogs.txt', level=logging.WARN)
+
 # MODULE METHODS
-
 class Character:
-
-	"""<Class "dark_souls_tabletop_character_tool/main/Character(object)"	"""
-
+	"""<Class "dark_souls_tabletop_character_tool/main/Character(object)"
+	"""
 	def __init__(self):
-
 		"""<Method "dark_souls_tabletop_character_tool/main.Character(object).__init__()">
-
 		Description:
-
 		Creates an instance of the Character() class.
-
 		"""
-
-		self.attunement = roll(1, 6) + 12
-
-		self.dexterity = roll(1, 6) + 12
-
-		self.endurance= roll(1, 6) + 12
-
-		self.faith = roll(1, 6) + 12
-
-		self.intelligence = roll(1, 6) + 12
-
-		self.strength = roll(1, 6) + 12
-
-		self.vitality = roll(1, 6) + 12
-
+		#? This internal function exists solely to add to the readability of __init__()
+		logging.info('A new instance of <Class "main.Character(object)"> is being created!')
+		def _stat_roll():
+			return roll(1, 6) + roll(1, 6) + 12
+		self.attunement = _stat_roll()
+		logging.debug('Attunement Determined...')
+		self.dexterity = _stat_roll()
+		logging.debug('Dexterity Determined...')
+		self.endurance= _stat_roll()
+		logging.debug('Endurance Determined...')
+		self.faith = _stat_roll()
+		logging.debug('Faith Determined...')
+		self.intelligence = _stat_roll()
+		logging.debug('Intelligence Determined...')
+		self.strength = _stat_roll()
+		logging.debug('Strength Determined...')
+		self.vitality = _stat_roll()
+		logging.debug('Vitality Determined...')
+		logging.debug('Instance initialization completed successfully.')
 	def __str__(self):
-
 		"""<Method "dark_souls_tabletop_character_tool/main.Character(object).__str__()">
-
 		Description:
-
 		This method handles the printability of the class and its attributes.
-
 		"""
-
-		_stats = (f'Attunememt: {self.attunement}',
-
-			f'Dexterity: {self.dexterity}',
-
-			f'Endurance: {self.endurance}',
-
-			f'Faith: {self.faith}',
-
-			f'Intelligence: {self.intelligence}',
-
-			f'Strength: {self.strength}',
-
-			f'Vitality: {self.vitality}')
-
+		logging.info('<Class "main.Character(object)"> is being printed as a string...')
+		_stats = (f'Attunememt: {self.attunement} (AB +{str(self.attunement)[0]})',
+			f'Dexterity: {self.dexterity} (DB +{str(self.dexterity)[0]})',
+			f'Endurance: {self.endurance} (EB +{str(self.endurance)[0]})',
+			f'Faith: {self.faith} (FB +{str(self.faith)[0]})',
+			f'Intelligence: {self.intelligence} (IB +{str(self.intelligence)[0]})',
+			f'Strength: {self.strength} (SB +{str(self.strength)[0]})',
+			f'Vitality: {self.vitality} (VB +{str(self.vitality)[0]})')
 		_output = '\n'.join(_stats)
-
+		logging.debug('main.character(object).__str__() completed successfully.')
 		return _output
 
 def main():
-
 	"""<Method "dark_souls_tabletop_character_tool/main.main()">
-
 	Description:
-
 	This method contains the main logic when this module is run directly.
-
 	"""
-
 	_test_char = Character()
-
+	print('Generating Dark Souls Character...\n')
 	print(_test_char)
 
 # DRIVER CODE
-
 if __name__ == '__main__':
-
 	main()
-
+	
+	while True:
+		_again = input("\nWould you like to run this program again?\n[Y/N]\n")
+		try:
+			if _again.lower() == 'y' or _again.lower() == 'yes':
+				print('\n')
+				main()
+			elif _again.lower() == 'n' or _again.lower() == 'no':
+				break
+			else:
+				raise ValueError('Invalid Response To "_again" Prompt.')
+		except ValueError as e:
+			logging.error(e)
+			continue
 else:
-
 	print(f'{__name__} has been successfully imported.')
