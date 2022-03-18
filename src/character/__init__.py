5ab65a05f27720c80280054972da3749d3050eae
TheOGMiyagi <69600897+TheOGMiyagi@users.noreply.github.com>
2022-03-18 16:05:26 -0700

    Update and rename src/__init__.py to src/character/__init__.py
    Fixed typo and created folder

--- src/character/__init__.py
+++ src/character/__init__.py
@@ -1,6 +1,6 @@
 """<Module "dark_souls_tabletop_character_tool/src/character/__init__.py"
 Description:
-This file initializes the /src/ package.
+This file initializes the /src/character/ package.
 """
 from .base_class import Character
 
